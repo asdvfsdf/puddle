561906e41cb68c5333c2b8b620c5b16577fc54b0
cola2003 <1952088712@qq.com>
2025-03-04 11:41:11 +0800

    db_log

--- index.php
+++ index.php
@@ -323,13 +323,14 @@
                                             $dbname = "puddle"; // 替换为你的数据库名称
 
                                             // 创建连接
-                                            $conn = new mysqli($servername, $username, $password);
+                                            $conn = new mysqli($servername, $username, $password, $dbname);
 
                                             // 检查连接
                                             if ($conn->connect_error) {
-                                            die("连接失败: " . $conn->connect_error);
+                                                die("连接失败: " . $conn->connect_error);
+                                            }else{
+                                                error_log("数据库连接成功", 3, "db_log.txt");
                                             }
-                                            echo "连接成功";
                                             ?>
 
                                         </body>
