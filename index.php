bdf1f9f73d1c795556347b425405de0928e75dae
cola2003 <1952088712@qq.com>
2025-03-06 19:42:44 +0800

    add form

--- index.php
+++ index.php
@@ -294,14 +294,12 @@ $visit_count = $redis->get($page_key);
                 });
             </script>
 
-            <body>
-                <div style="text-align: right; padding: 20px;">
-                    <form action="search.php" method="get">
-                        <input type="text" name="query" placeholder="请输入搜索内容" required>
-                        <button type="submit">搜索</button>
-                    </form>
-                </div>
-            </body>
+            <div style="text-align: right; padding: 20px;">
+                <form action="search.php" method="get">
+                    <input type="text" name="query" placeholder="请输入搜索内容" required>
+                    <button type="submit">搜索</button>
+                </form>
+            </div>
 
             <div style="display: flex;justify-content: center;align-items: center;">
                 <div class="row bwb-detail-row">
